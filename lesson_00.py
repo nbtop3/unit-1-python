420174ac4590283fff04cf56bd58d977463dadbb
nbtop3 <nbristol@brooklynsteamcenter.a>
2024-09-16 10:47:30 -0400

    benchmark

---
+++ lesson_00.py
@@ -0,0 +1,10 @@
+print("nehemiah")
+#my first step
+fav_show_=("shameless")
+#my fav show
+float(25.00)
+#making a float step
+int(50)
+#making a integer step
+(pizza,chicken,curry goat)
+#my favorite foods
