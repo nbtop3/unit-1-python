b1e80488a905b1f1e9d515534cdd7507a24f5c37
nbtop3 <nbristol@brooklynsteamcenter.a>
2024-09-17 10:51:10 -0400

    data types

---
+++ 01_data_types.py
@@ -0,0 +1,44 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+a= 15.50
+b=int(a) #Converting my variable to an integer
+print(a)
+print(b)
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+[-3,-2,-1,0,1,2,3]
+input(1) #The number that is printing
+a = 1
+b = 3
+
+if b > a:
+  print("b is greater than a")
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+fruits = ('Apples' : )
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
