73f447c0dba3c08a174febb8bc5001826706fa86
nbtop3 <nbristol@brooklynsteamcenter.a>
2024-10-07 10:52:09 -0400

    duibious

---
+++ 10_dubious_dates.py
@@ -0,0 +1,27 @@
+"""
+Exercise 1:
+Write a Python program that prints the current date and time using the datetime module.
+"""
+from datetime import date
+my_date = date.today ()
+my_string = my_date.
+
+"""
+Exercise 2:
+Write a Python program that prints the current date and time using the datetime module.
+Using the strftime function format the date in standard U.S. date format (MM/DD/YYYY)
+"""
+from datetime import date 
+my_date = date.today()
+my_string = my_date.striftime
+"""
+Exercise 3:
+Using the strptime function, convert two strings into dates.
+Then find the difference in days between the two.
+"""
+
+"""
+Excercise 4:
+Write a program that asks the user for their birthdate and calculates their current 
+age using the datetime module.
+"""
